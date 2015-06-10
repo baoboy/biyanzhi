404b3aeeab979ccf0a204bb16079a6179f258857
songbinbin <774663576@qq.com>
2015-06-10 22:02:23 +0800

    a

--- src/com/biyanzhi/activity/UserInfoInfoView.java
+++ src/com/biyanzhi/activity/UserInfoInfoView.java
@@ -11,6 +11,7 @@ public class UserInfoInfoView {
 	private TextView txt_gender;
 	private TextView txt_birthday;
 	private TextView txt_address;
+	private TextView txt_guanzhu;
 
 	public UserInfoInfoView(UserInfoActivity activity, View contentRootView) {
 		this.mActivity = activity;
@@ -19,6 +20,8 @@ public class UserInfoInfoView {
 	}
 
 	private void initView() {
+		txt_guanzhu = (TextView) mContentRootView
+				.findViewById(R.id.txt_guanzhu);
 		txt_address = (TextView) mContentRootView
 				.findViewById(R.id.txt_address);
 		txt_birthday = (TextView) mContentRootView
@@ -28,9 +31,10 @@ public class UserInfoInfoView {
 	}
 
 	public void setValue(String str_address, String str_gender,
-			String str_birthday) {
+			String str_birthday, int guanzhu_count) {
 		txt_address.setText(str_address);
 		txt_birthday.setText(str_birthday);
 		txt_gender.setText(str_gender);
+		txt_guanzhu.setText(guanzhu_count + "»À");
 	}
 }
